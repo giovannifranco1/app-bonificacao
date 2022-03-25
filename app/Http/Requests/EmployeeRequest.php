810d02d9ee267314197327c9748ec3542d4ca4a1
giovannifranco1 <giovannifrancorezende2012@gmail.com>
2022-03-25 14:24:18 -0300

    :hammer_and_pick: Criação EmployeeRequest

---
+++ app/Http/Requests/EmployeeRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EmployeeRequest extends FormRequest
+{
+  /**
+   * Determine if the user is authorized to make this request.
+   *
+   * @return bool
+   */
+  public function authorize()
+  {
+    return true;
+  }
+
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array
+   */
+  public function rules()
+  {
+    return [
+      'full_name' => ['required', 'max:300', 'min:0', 'string'],
+      'login' => ['required', 'string', 'max:300', 'min:0'],
+      'password' => ['required', 'string', 'max:200', 'min:0'],
+      'administrator_id' => ['required', 'integer'],
+    ];
+  }
+}
