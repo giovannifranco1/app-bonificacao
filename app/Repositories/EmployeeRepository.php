68e4251874c1f146326134523accd152d5ce6766
giovannifranco1 <giovannifrancorezende2012@gmail.com>
2022-03-25 14:12:27 -0300

    :hammer_and_pick: Criação Service + Repository de Employee

---
+++ app/Repositories/EmployeeRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Repositories\Interfaces\EmployeeRepositoryInterface;
+
+class EmployeeRepository implements EmployeeRepositoryInterface
+{
+    public function __construct()
+    {
+        //
+    }
+}
---
+++ app/Services/EmployeeService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Services;
+
+class EmployeeService
+{
+    public function __construct()
+    {
+        //
+    }
+}
