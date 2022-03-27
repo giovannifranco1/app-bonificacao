dfd1784e38ee6d1d009fb1945e0dce2fa5d08f5c
giovannifranco1 <giovannifrancorezende2012@gmail.com>
2022-03-27 15:14:58 -0300

    :construction: Criação MovementController

---
+++ app/Http/Controllers/Admin/MovementController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+
+class MovementController extends Controller
+{
+  public function index()
+  {
+    # code...
+  }
+
+  public function create()
+  {
+    # code...
+  }
+
+  public function store()
+  {
+    # code...
+  }
+
+  public function showByEmployee()
+  {
+    # code...
+  }
+
+  public function search()
+  {
+    # code...
+  }
+}
