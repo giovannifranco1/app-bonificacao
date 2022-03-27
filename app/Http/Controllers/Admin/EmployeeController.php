5b2811a2bc0ffa42f31db68c4a7ee7593307284d
giovannifranco1 <giovannifrancorezende2012@gmail.com>
2022-03-27 11:59:20 -0300

    :construction_worker: Criação funções edit + update EmployeeController

--- app/Http/Controllers/Admin/EmployeeController.php
+++ app/Http/Controllers/Admin/EmployeeController.php
@@ -6,10 +6,13 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\EmployeeRequest;
 use App\Repositories\Interfaces\EmployeeRepositoryInterface;
 use App\Services\EmployeeService;
+use DateTime;
 use Exception;
 use Illuminate\Contracts\View\View;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 
 class EmployeeController extends Controller
 {
@@ -33,7 +36,7 @@ class EmployeeController extends Controller
   public function index(): View
   {
     $relations = collect('administrator');
-    $employees = $this->employeeRepo->paginate(10, 'full_name', $relations->toArray());
+    $employees = $this->employeeRepo->paginate(25, 'full_name', [], $relations->toArray());
 
     return view('admin.employee.index', compact('employees'));
   }
@@ -59,10 +62,15 @@ class EmployeeController extends Controller
     try {
       $this->employeeService->storeOrUpdate(collect($request->validated()));
     } catch (Exception $e) {
-      return redirect()->back()->withErrors($e->getMessage());
+      report($e);
+      return redirect()
+        ->back()
+        ->withInput($request->all())
+        ->withErrors(['Error registering employee, I apologize we are working to resolve it as soon as possible.']);
     }
-
-    return redirect()->back();
+    return redirect()
+      ->back()
+      ->with('success', 'successfully created');
   }
 
   /**
@@ -73,7 +81,16 @@ class EmployeeController extends Controller
    */
   public function show($id)
   {
-    //
+    try {
+      $this->employeeRepo->findBy('id', $id);
+    } catch (ModelNotFoundException $e) {
+      throw $e;
+    } catch (Exception $e) {
+      report($e);
+      return redirect()
+        ->back()
+        ->withErrors(['Error find employee, I apologize we are working to resolve it as soon as possible.']);
+    }
   }
 
   /**
@@ -85,11 +102,16 @@ class EmployeeController extends Controller
   public function edit($id)
   {
     try {
-      $this->employeeRepo->findBy('id', $id);
+      $employee = $this->employeeRepo->findById($id);
     } catch (ModelNotFoundException $e) {
-      return redirect()->back()->withErrors($e->getMessage());
+      throw $e;
+    } catch (Exception $e) {
+      report($e);
+      return redirect()
+        ->back()
+        ->withErrors(['Error find employee, I apologize we are working to resolve it as soon as possible.']);
     }
-    return view();
+    return view('admin.employee.edit', compact('employee'));
   }
 
   /**
@@ -101,7 +123,17 @@ class EmployeeController extends Controller
    */
   public function update(EmployeeRequest $request, $id)
   {
-    //
+    try {
+      $this->employeeRepo->updateEmployee($id, $request->validated());
+    } catch (ModelNotFoundException $e) {
+      throw $e;
+    } catch (Exception $e) {
+      report($e);
+      return redirect()
+        ->back()
+        ->withErrors(['Error find employee, I apologize we are working to resolve it as soon as possible.']);
+    }
+    return redirect()->back()->with('success', 'successfully deleted');
   }
 
   /**
@@ -110,8 +142,47 @@ class EmployeeController extends Controller
    * @param  int  $id
    * @return \Illuminate\Http\Response
    */
-  public function destroy($id)
+  public function destroy($id): RedirectResponse
   {
-    //
+    try {
+      $this->employeeRepo->deleteEmployee($id);
+    } catch (ModelNotFoundException $e) {
+      throw $e;
+    } catch (Exception $e) {
+      report($e);
+      return redirect()
+        ->back()
+        ->withErrors(['Error find employee, I apologize we are working to resolve it as soon as possible.']);
+    }
+    return redirect()->back()->with('success', 'successfully deleted');
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @return \Illuminate\Http\Response
+   */
+
+  public function search()
+  {
+    $filled = collect(request()->only([
+      'full_name',
+      'created_at',
+    ]))->filter();
+
+    if ($filled->get('created_at')) {
+      $filled->put('created_at', new DateTime((string) $filled->get('created_at')));
+    }
+
+    try {
+      $employees = $this->employeeRepo->listAll(25, $filled->toArray());
+    } catch (Exception $e) {
+      report($e);
+      return redirect()
+        ->back()
+        ->withErrors(['Error search employee, I apologize we are working to resolve it as soon as possible.']);
+    }
+    $inputs = request()->all();
+    return view('admin.employee.index', compact('employees', 'inputs'));
   }
 }
